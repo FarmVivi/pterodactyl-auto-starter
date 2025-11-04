f827e0de60e6b5c8626c7f9b101726115268625b
FarmVivi <farmvividev@gmail.com>
2025-11-04 20:46:47 +0100

    refactor: remplacer l'utilisation de Logger par un délégué dans LoggerProxy pour une meilleure clarté et cohérence

--- pterodactyl-auto-starter-common/src/main/java/fr/farmvivi/pterodactylautostarter/LoggerProxy.java
+++ pterodactyl-auto-starter-common/src/main/java/fr/farmvivi/pterodactylautostarter/LoggerProxy.java
@@ -9,354 +9,355 @@ import java.util.logging.Filter;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
-public class LoggerProxy extends java.util.logging.Logger {
-    private final java.util.logging.Logger logger;
+public class LoggerProxy extends Logger {
+    private final Logger delegate;
 
-    public LoggerProxy(java.util.logging.Logger logger) {
+    public LoggerProxy(Logger logger) {
         super(logger.getName(), logger.getResourceBundleName());
-        this.logger = logger;
+        this.delegate = logger;
     }
 
     @Override
     public ResourceBundle getResourceBundle() {
-        return logger.getResourceBundle();
+        return delegate.getResourceBundle();
     }
 
     @Override
     public void setResourceBundle(ResourceBundle bundle) {
-        logger.setResourceBundle(bundle);
+        delegate.setResourceBundle(bundle);
     }
 
     @Override
     public String getResourceBundleName() {
-        return logger.getResourceBundleName();
+        return delegate.getResourceBundleName();
     }
 
     @Override
     public Filter getFilter() {
-        return logger.getFilter();
+        return delegate.getFilter();
     }
 
     @Override
     public void setFilter(Filter newFilter) throws SecurityException {
-        logger.setFilter(newFilter);
+        delegate.setFilter(newFilter);
     }
 
     @Override
     public void log(LogRecord record) {
-        logger.log(record);
+        delegate.log(record);
     }
 
     @Override
     public void log(Level level, String msg) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, msg);
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, msg);
         }
     }
 
     public void log(Level level, Component component) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, convertComponentToLogacyComponent(component));
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, convertComponentToLogacyComponent(component));
         }
     }
 
     @Override
     public void log(Level level, Supplier<String> msgSupplier) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, msgSupplier);
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, msgSupplier);
         }
     }
 
     @Override
     public void log(Level level, String msg, Object param1) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, msg, param1);
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, msg, param1);
         }
     }
 
     public void log(Level level, Component component, Object param1) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, convertComponentToLogacyComponent(component), param1);
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, convertComponentToLogacyComponent(component), param1);
         }
     }
 
     @Override
     public void log(Level level, String msg, Object[] params) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, msg, params);
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, msg, params);
         }
     }
 
     public void log(Level level, Component component, Object[] params) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, convertComponentToLogacyComponent(component), params);
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, convertComponentToLogacyComponent(component), params);
         }
     }
 
     @Override
     public void log(Level level, String msg, Throwable thrown) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, msg, thrown);
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, msg, thrown);
         }
     }
 
     public void log(Level level, Component component, Throwable thrown) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, convertComponentToLogacyComponent(component), thrown);
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, convertComponentToLogacyComponent(component), thrown);
         }
     }
 
     @Override
     public void log(Level level, Throwable thrown, Supplier<String> msgSupplier) {
-        if (logger.isLoggable(level)) {
-            logger.log(level, thrown, msgSupplier);
+        if (delegate.isLoggable(level)) {
+            delegate.log(level, thrown, msgSupplier);
         }
     }
 
     @Override
     public void logp(Level level, String sourceClass, String sourceMethod, String msg) {
-        logger.logp(level, sourceClass, sourceMethod, msg);
+        delegate.logp(level, sourceClass, sourceMethod, msg);
     }
 
     public void logp(Level level, String sourceClass, String sourceMethod, Component component) {
-        logger.logp(level, sourceClass, sourceMethod, convertComponentToLogacyComponent(component));
+        delegate.logp(level, sourceClass, sourceMethod, convertComponentToLogacyComponent(component));
     }
 
     @Override
     public void logp(Level level, String sourceClass, String sourceMethod, Supplier<String> msgSupplier) {
-        logger.logp(level, sourceClass, sourceMethod, msgSupplier);
+        delegate.logp(level, sourceClass, sourceMethod, msgSupplier);
     }
 
     @Override
     public void logp(Level level, String sourceClass, String sourceMethod, String msg, Object param1) {
-        logger.logp(level, sourceClass, sourceMethod, msg, param1);
+        delegate.logp(level, sourceClass, sourceMethod, msg, param1);
     }
 
     public void logp(Level level, String sourceClass, String sourceMethod, Component component, Object param1) {
-        logger.logp(level, sourceClass, sourceMethod, convertComponentToLogacyComponent(component), param1);
+        delegate.logp(level, sourceClass, sourceMethod, convertComponentToLogacyComponent(component), param1);
     }
 
     @Override
     public void logp(Level level, String sourceClass, String sourceMethod, String msg, Object[] params) {
-        logger.logp(level, sourceClass, sourceMethod, msg, params);
+        delegate.logp(level, sourceClass, sourceMethod, msg, params);
     }
 
     public void logp(Level level, String sourceClass, String sourceMethod, Component component, Object[] params) {
-        logger.logp(level, sourceClass, sourceMethod, convertComponentToLogacyComponent(component), params);
+        delegate.logp(level, sourceClass, sourceMethod, convertComponentToLogacyComponent(component), params);
     }
 
     @Override
     public void logp(Level level, String sourceClass, String sourceMethod, String msg, Throwable thrown) {
-        logger.logp(level, sourceClass, sourceMethod, msg, thrown);
+        delegate.logp(level, sourceClass, sourceMethod, msg, thrown);
     }
 
     public void logp(Level level, String sourceClass, String sourceMethod, Component component, Throwable thrown) {
-        logger.logp(level, sourceClass, sourceMethod, convertComponentToLogacyComponent(component), thrown);
+        delegate.logp(level, sourceClass, sourceMethod, convertComponentToLogacyComponent(component), thrown);
     }
 
     @Override
     public void logp(Level level, String sourceClass, String sourceMethod, Throwable thrown, Supplier<String> msgSupplier) {
-        if (logger.isLoggable(level)) {
-            logger.logp(level, sourceClass, sourceMethod, thrown, msgSupplier);
+        if (delegate.isLoggable(level)) {
+            delegate.logp(level, sourceClass, sourceMethod, thrown, msgSupplier);
         }
     }
 
     @Override
     public void entering(String sourceClass, String sourceMethod) {
-        logger.entering(sourceClass, sourceMethod);
+        delegate.entering(sourceClass, sourceMethod);
     }
 
     @Override
     public void entering(String sourceClass, String sourceMethod, Object param1) {
-        logger.entering(sourceClass, sourceMethod, param1);
+        delegate.entering(sourceClass, sourceMethod, param1);
     }
 
     @Override
     public void entering(String sourceClass, String sourceMethod, Object[] params) {
-        logger.entering(sourceClass, sourceMethod, params);
+        delegate.entering(sourceClass, sourceMethod, params);
     }
 
     @Override
     public void exiting(String sourceClass, String sourceMethod) {
-        logger.exiting(sourceClass, sourceMethod);
+        delegate.exiting(sourceClass, sourceMethod);
     }
 
     @Override
     public void exiting(String sourceClass, String sourceMethod, Object result) {
-        logger.exiting(sourceClass, sourceMethod, result);
+        delegate.exiting(sourceClass, sourceMethod, result);
     }
 
     @Override
     public void throwing(String sourceClass, String sourceMethod, Throwable thrown) {
-        logger.throwing(sourceClass, sourceMethod, thrown);
+        delegate.throwing(sourceClass, sourceMethod, thrown);
     }
 
     @Override
     public void severe(String msg) {
-        logger.severe(msg);
+        delegate.severe(msg);
     }
 
     public void severe(Component component) {
-        if (logger.isLoggable(Level.SEVERE)) {
-            logger.severe(convertComponentToLogacyComponent(component));
+        if (delegate.isLoggable(Level.SEVERE)) {
+            delegate.severe(convertComponentToLogacyComponent(component));
         }
     }
 
     @Override
     public void warning(String msg) {
-        logger.warning(msg);
+        delegate.warning(msg);
     }
 
     public void warning(Component component) {
-        if (logger.isLoggable(Level.WARNING)) {
-            logger.warning(convertComponentToLogacyComponent(component));
+        if (delegate.isLoggable(Level.WARNING)) {
+            delegate.warning(convertComponentToLogacyComponent(component));
         }
     }
 
     @Override
     public void info(String msg) {
-        logger.info(msg);
+        delegate.info(msg);
     }
 
     public void info(Component component) {
-        if (logger.isLoggable(Level.INFO)) {
-            logger.info(convertComponentToLogacyComponent(component));
+        if (delegate.isLoggable(Level.INFO)) {
+            delegate.info(convertComponentToLogacyComponent(component));
         }
     }
 
     @Override
     public void config(String msg) {
-        logger.config(msg);
+        delegate.config(msg);
     }
 
     public void config(Component component) {
-        if (logger.isLoggable(Level.CONFIG)) {
-            logger.config(convertComponentToLogacyComponent(component));
+        if (delegate.isLoggable(Level.CONFIG)) {
+            delegate.config(convertComponentToLogacyComponent(component));
         }
     }
 
     @Override
     public void fine(String msg) {
-        logger.fine(msg);
+        delegate.fine(msg);
     }
 
     public void fine(Component component) {
-        if (logger.isLoggable(Level.FINE)) {
-            logger.fine(convertComponentToLogacyComponent(component));
+        if (delegate.isLoggable(Level.FINE)) {
+            delegate.fine(convertComponentToLogacyComponent(component));
         }
     }
 
     @Override
     public void finer(String msg) {
-        logger.finer(msg);
+        delegate.finer(msg);
     }
 
     public void finer(Component component) {
-        if (logger.isLoggable(Level.FINER)) {
-            logger.finer(convertComponentToLogacyComponent(component));
+        if (delegate.isLoggable(Level.FINER)) {
+            delegate.finer(convertComponentToLogacyComponent(component));
         }
     }
 
     @Override
     public void finest(String msg) {
-        logger.finest(msg);
+        delegate.finest(msg);
     }
 
     public void finest(Component component) {
-        if (logger.isLoggable(Level.FINEST)) {
-            logger.finest(convertComponentToLogacyComponent(component));
+        if (delegate.isLoggable(Level.FINEST)) {
+            delegate.finest(convertComponentToLogacyComponent(component));
         }
     }
 
     @Override
     public void severe(Supplier<String> msgSupplier) {
-        logger.severe(msgSupplier);
+        delegate.severe(msgSupplier);
     }
 
     @Override
     public void warning(Supplier<String> msgSupplier) {
-        logger.warning(msgSupplier);
+        delegate.warning(msgSupplier);
     }
 
     @Override
     public void info(Supplier<String> msgSupplier) {
-        logger.info(msgSupplier);
+        delegate.info(msgSupplier);
     }
 
     @Override
     public void config(Supplier<String> msgSupplier) {
-        logger.config(msgSupplier);
+        delegate.config(msgSupplier);
     }
 
     @Override
     public void fine(Supplier<String> msgSupplier) {
-        logger.fine(msgSupplier);
+        delegate.fine(msgSupplier);
     }
 
     @Override
     public void finer(Supplier<String> msgSupplier) {
-        logger.finer(msgSupplier);
+        delegate.finer(msgSupplier);
     }
 
     @Override
     public void finest(Supplier<String> msgSupplier) {
-        logger.finest(msgSupplier);
+        delegate.finest(msgSupplier);
     }
 
     @Override
     public Level getLevel() {
-        return logger.getLevel();
+        return delegate.getLevel();
     }
 
     @Override
     public void setLevel(Level newLevel) throws SecurityException {
-        logger.setLevel(newLevel);
+        delegate.setLevel(newLevel);
     }
 
     @Override
     public boolean isLoggable(Level level) {
-        return logger.isLoggable(level);
+        return delegate.isLoggable(level);
     }
 
     @Override
     public String getName() {
-        return logger.getName();
+        return delegate.getName();
     }
 
     @Override
     public void addHandler(Handler handler) throws SecurityException {
-        logger.addHandler(handler);
+        delegate.addHandler(handler);
     }
 
     @Override
     public void removeHandler(Handler handler) throws SecurityException {
-        logger.removeHandler(handler);
+        delegate.removeHandler(handler);
     }
 
     @Override
     public Handler[] getHandlers() {
-        return logger.getHandlers();
+        return delegate.getHandlers();
     }
 
     @Override
     public boolean getUseParentHandlers() {
-        return logger.getUseParentHandlers();
+        return delegate.getUseParentHandlers();
     }
 
     @Override
     public void setUseParentHandlers(boolean useParentHandlers) {
-        logger.setUseParentHandlers(useParentHandlers);
+        delegate.setUseParentHandlers(useParentHandlers);
     }
 
     @Override
-    public java.util.logging.Logger getParent() {
-        return logger.getParent();
+    public Logger getParent() {
+        return delegate.getParent();
     }
 
     @Override
-    public void setParent(java.util.logging.Logger parent) {
-        logger.setParent(parent);
+    public void setParent(Logger parent) {
+        delegate.setParent(parent);
     }
 
     public String convertComponentToLogacyComponent(Component component) {
@@ -365,16 +366,16 @@ public class LoggerProxy extends java.util.logging.Logger {
 
     @Override
     public int hashCode() {
-        return logger.hashCode();
+        return delegate.hashCode();
     }
 
     @Override
     public boolean equals(Object obj) {
-        return logger.equals(obj);
+        return delegate.equals(obj);
     }
 
     @Override
     public String toString() {
-        return logger.toString();
+        return delegate.toString();
     }
 }
